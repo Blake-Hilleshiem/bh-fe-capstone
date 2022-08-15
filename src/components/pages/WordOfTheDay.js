3d8ba3f91bd6cfe60bdc2c46266bacda4f73a71b
Blake-Hilleshiem <blake@devpipeline.com>
2022-08-15 07:41:18 -0600

    random word generator working

--- src/components/pages/WordOfTheDay.js
+++ src/components/pages/WordOfTheDay.js
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
+
+import words from "../../data/words";
+
 function WordOfTheDay() {
+  const [selectedWord, setSelectedWord] = useState("await");
+  const [definition, setDefinition] = useState("");
+
+  function getWordIndex() {
+    return Math.floor(Math.random() * words.length);
+  }
+
+  function handleOnClick() {
+    setSelectedWord(words[getWordIndex()]);
+  }
+
+  useEffect(() => {
+    setSelectedWord(words[getWordIndex()]);
+  }, []);
+
+  useEffect(() => {}, [selectedWord]);
+
+  // console.log(words[getWordIndex()]);
+  // console.log(words);
   return (
     <div>
       <h1>Hello from WordOfTheDay</h1>
+      <div>{selectedWord}</div>
+      <div>{definition}</div>
+      <button onClick={handleOnClick}>New Word</button>
     </div>
   );
 }
