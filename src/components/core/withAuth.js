f665de5925c1f734866fca3364890dd71e7ae547
Blake-Hilleshiem <blake@devpipeline.com>
2022-08-17 17:10:04 -0600

    missed a comment

--- src/components/core/withAuth.js
+++ src/components/core/withAuth.js
@@ -6,7 +6,6 @@ import { UserContext } from "../../UserProvider";
 export default function withAuth(authorizedRoles) {
   return ({ children, withRedirect }) => {
     const { user } = useContext(UserContext);
-    // console.log("AUTH:", user);
     if (authorizedRoles.includes(user?.role)) {
       return children;
     } else {
